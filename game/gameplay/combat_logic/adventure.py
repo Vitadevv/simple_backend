df9ab2144e14fa3a0978f8bf6641802dbaf9de2c
Vitadevv <10qwertyabcdef.com>
2025-11-04 19:53:28 +0000

    adventure skeleton

--- game/gameplay/combat_logic/adventure.py
+++ game/gameplay/combat_logic/adventure.py
@@ -1,15 +1,33 @@
 from ..entities.entities import Entity 
 
 
-def take_turn(player: Entity, enemy: Entity):
-"""Executes only 1 turn. Useful for debugging and may be a useful helper function"""
-  if enemy.hp > 0:
+def take_turn(player: Entity, enemy: Entity) -> dict:
+"""Executes only 1 turn. Useful for debugging and may be a useful helper function."""
+  report = []
+  player_hit = player.attack_target(enemy)
+  report.append(player_hit)
+  if enemy.is_alive():
+    enemy_hit = enemy.attack_target(player)
+    report.append(enemy_hit)
+  return {
+  "player HP": player.hp,
+  "enemy HP": enemy.hp,
+  "report": report
+  
+  }
   
 def fight_to_death(player: Entity, enemy: Entity) -> dict:
   turns = 0
-  log = []
-  while player.hp > 0 and enemy.hp > 0:
-    take_turn(player, enemy)
-    log.extend(f"Player: )
-  
+  report = []
+  while player.is_alive() and enemy.is_alive():
+    turn = take_turn(player, enemy)
+    report.append(turn)
     turns += 1
+  winner = player.name if player.is_alive() else enemy.name
+  player_remaining_hp = player.hp if player.is_alive() else 0
+  return {
+  "winner": winner,
+  "remaining_hp": player_remaining_hp,
+  "turns": turns,
+  "report": report
+  }
